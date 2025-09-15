324c56cf2279fb85cb2db9bf3fd69dae2c94d289
AlessandroDJ06 <alessandrodejongh@outlook.com>
2025-09-15 14:48:02 +0200

    commit1

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        int testVariabel= 50;
+        System.out.println(testVariabel);
+        System.out.println("test");
+
+    }    
+}
