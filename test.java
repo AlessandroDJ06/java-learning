f4a415f24322b2520cda91ede04302b4a53a3362
AlessandroDJ06 <alessandrodejongh@outlook.com>
2025-09-17 00:05:41 +0200

    week1 oefeningen deel1

--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        int testVariabel= 50;
-        System.out.println(testVariabel);
-        System.out.println("test");
-
-    }    
-}
