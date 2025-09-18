ae778b2cb48c1b1af1ea4cefc3a9fb3a281eadd2
AlessandroDJ06 <alessandrodejongh@outlook.com>
2025-09-18 23:33:59 +0200

    commit3

---
+++ week1/src/kassa.java
@@ -0,0 +1,56 @@
+package week1.src;
+import java.util.Scanner;
+public class kassa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double korting;
+        int kleinekinderen;
+        int kinderen;
+        int volwassenen;
+        int senioren;
+        int totaalbetalendemensen;
+        double prijsformule;
+        double prijs;
+        int aantalmensen;
+
+        System.out.print("Aantal kinderen <1m: ");
+        kleinekinderen = scanner.nextInt();
+        System.out.print("Aantal kinderen tussen 1m en 1,40m: ");
+        kinderen = scanner.nextInt();
+        System.out.print("Aantal volwassenen: ");
+        volwassenen = scanner.nextInt();
+        System.out.print("Aantal senioren (55+): ");
+        senioren = scanner.nextInt();
+
+        totaalbetalendemensen = kinderen + volwassenen + senioren;
+        aantalmensen = kinderen + kleinekinderen +  volwassenen + senioren;
+        prijsformule = (26*kinderen) + (31*volwassenen) + (15.50*senioren);
+        System.out.println("\n*******************************");
+        System.out.println("* Welkom in het pretpark! *");
+        System.out.println("*******************************");
+
+        if (totaalbetalendemensen >= 10){
+            korting = prijsformule * 0.10;
+            prijs = prijsformule - korting;
+            System.out.printf("*Prijs: %.1fEUR",prijsformule);
+            System.out.printf("\n*groepskorting: %.1fEUR",korting);
+            System.out.printf("\n*Totaal: %.1fEUR",prijs);
+            System.out.println("\n*******************************");
+
+
+        } else if (senioren >= 2) {
+            korting = 13*kinderen;
+            prijs = prijsformule-korting;
+            System.out.printf("*Prijs: %.1f EUR",prijsformule);
+            System.out.printf("\n*Grootouderskorting: %.1fEUR",korting);
+            System.out.printf("\n*Totaal: %.1f EUR",prijs);
+            System.out.println("\n*******************************");
+
+        }else {
+            System.out.printf("*Totaal: %.1f EUR",prijsformule);
+            System.out.println("\n*******************************");
+
+        }
+        System.out.printf("*totaal aantal mensen: %d *",aantalmensen);
+    }
+}
---
+++ week1/src/tafels2.java
@@ -0,0 +1,42 @@
+package week1.src;
+import java.util.Scanner;
+public class tafels2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int gevraagdetafel;
+        int count = 0;
+        int fouten = 0;
+        int gok;
+        int echteScore;
+        String doorgaan = "j";
+
+
+
+        while (doorgaan.equals("j")){
+            System.out.print("van welk getal wil je de tafel weten?: ");
+            gevraagdetafel = scanner.nextInt();
+            while(count != 11){
+                System.out.println("wat is " + count + " * " + gevraagdetafel + " = ...");
+                gok = scanner.nextInt();
+                echteScore = count * gevraagdetafel;
+                while(gok != echteScore){
+                    System.out.println("dat is helaas fout... probeer opnieuw :) " +  count + " * " + gevraagdetafel + " = ...");
+                    gok = scanner.nextInt();
+                    fouten++;
+                }
+                System.out.println("dat is correct!");
+                count++;
+            }
+            count = 0;
+            System.out.println("je maakte " + fouten + " fouten op de tafel van " + gevraagdetafel + ".");
+            fouten = 0;
+            System.out.print("wil je een andere tafel hebben?(j/n): ");
+            doorgaan = scanner.next();
+
+        }
+
+
+        scanner.close();
+    }
+}
