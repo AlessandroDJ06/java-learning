47b2dfcaa86602d8a2f1190641774ecb9dcfaf61
AlessandroDJ06 <alessandrodejongh@outlook.com>
2025-09-18 20:58:52 +0200

    commit3

---
+++ week1/src/Cowsay.java
@@ -0,0 +1,25 @@
+package week1.src;
+import java.util.Scanner;
+public class Cowsay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String zin;
+        int lengte;
+        System.out.print("geef een zin: ");
+        zin = scanner.nextLine();
+        lengte = zin.length();
+
+        String lijn = "*".repeat(lengte + 4);
+
+        System.out.println(lijn);
+        System.out.println(zin);
+        System.out.println(lijn);
+        System.out.println("     \\   ^__^");
+        System.out.println("      \\  (oo)\\_______");
+        System.out.println("         (__)\\       )\\/\\");
+        System.out.println("          U ||----w |");
+        System.out.println("            ||      ||");
+
+
+    }
+}
---
+++ week1/src/Reeksen.java
@@ -0,0 +1,26 @@
+package week1.src;
+import java.util.Scanner;
+public class Reeksen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int aantalGetallen;
+        double startWaarde;
+        double stapWaarde;
+
+        System.out.print("Hoeveel getallen wil je afdrukken? ");
+        aantalGetallen = scanner.nextInt();
+        System.out.print("met welke waarde wil je starten? ");
+        startWaarde = scanner.nextDouble();
+        System.out.print("met welke waarde wil je verhogen? ");
+        stapWaarde = scanner.nextDouble();
+
+        System.out.printf("%.0f ",startWaarde);
+        aantalGetallen --;
+        while (aantalGetallen != 0){
+            startWaarde += stapWaarde;
+            System.out.printf(" %.0f ",startWaarde);
+            aantalGetallen --;
+        }
+    }
+}
