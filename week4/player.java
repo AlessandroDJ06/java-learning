58bb2850ab62147b142d43f614b69e229ef95cf5
AlessandroDJ06 <alessandrodejongh@outlook.com>
2025-10-22 21:19:38 +0200

    oefening "highscores" aangepast

--- week4/player.java
+++ week4/player.java
@@ -20,6 +20,32 @@ public class player {
         if (score < 0) return;
         this.score = score;
     }
+
+    public static String sorteer(player player1,player player2,player player3) {
+        if (player1.getScore() > player2.getScore() && player1.getScore() > player3.getScore()) {
+            if (player2.getScore() > player3.getScore()) {
+                return player1.getName() + " " + player1.getScore() + "\n" + player2.getName() + " " + player2.getScore() + "\n" + player3.getName() + " " + player3.getScore();
+            } else {
+                return player1.getName() + " " + player1.getScore() + "\n" + player3.getName() + " " + player3.getScore() + "\n" + player2.getName() + " " + player2.getScore();
+            }
+        } else if (player2.getScore() > player1.getScore() && player2.getScore() > player3.getScore()) {
+            if (player1.getScore() > player3.getScore()) {
+                return player2.getName() + " " + player2.getScore() + "\n" + player1.getName() + " " + player1.getScore() + "\n" + player3.getName() + " " + player3.getScore();
+            } else {
+                return player2.getName() + " " + player2.getScore() + "\n" + player3.getName() + " " + player3.getScore() + "\n" + player1.getName() + " " + player1.getScore();
+            }
+
+        }else if (player3.getScore() > player1.getScore() && player3.getScore() > player2.getScore()) {
+            if (player1.getScore() > player2.getScore()) {
+                return player3.getName() + " " + player3.getScore() + "\n" + player1.getName() + " " + player1.getScore() + "\n" + player2.getName() + " " + player2.getScore();
+            } else {
+                return player3.getName() + " " + player3.getScore() + "\n" + player2.getName() + " " + player2.getScore() + "\n" + player1.getName() + " " + player1.getScore();
+            }
+
+        }else {
+            return "";
+        }
+    }
     public static void main(String[] args) {
         player speler1 = new player();
         player speler2 = new player();
@@ -35,9 +61,8 @@ public class player {
 
         System.out.println("HIGHSCORES");
         System.out.println("----------");
-        System.out.println(speler1.getName() + " " + speler1.getScore());
-        System.out.println(speler2.getName() + " " + speler2.getScore());
-        System.out.println(speler3.getName() + " " + speler3.getScore());
+        System.out.println(sorteer(speler1,speler2,speler3));
+
 
 
     }
